a5a9f266884270949a8d2a9498157178f30db7de
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-02-04 22:12:52 +0800

    Add and connect static and media folders

--- issue_tracker/settings.py
+++ issue_tracker/settings.py
@@ -64,7 +64,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-          os.path.join(BASE_DIR, 'template'),
+          os.path.join(BASE_DIR, 'templates'),
           os.path.join(BASE_DIR, 'templates', 'allauth')
         ],
         'APP_DIRS': True,
@@ -150,6 +150,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
