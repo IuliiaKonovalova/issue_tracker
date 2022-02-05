63448e8befaf53a7fdb19eb4675583938b4844da
lexach91 <ronmctailor@protonmail.com>
2022-02-05 15:09:06 +0800

    Add issue and comment models to projects app

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,77 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# models for issue tracker app
+
+ISSUE_STATUS = (
+    ('0', 'Open'),
+    ('1', 'Closed'),
+    ('2', 'In Progress'),
+)
+
+ISSUE_PRIORITY = (
+    ('0', 'Low'),
+    ('1', 'Medium'),
+    ('2', 'High'),
+    ('3', 'Urgent'),
+)
+
+ISSUE_TYPE = (
+    ('0', 'Bug'),
+    ('1', 'Feature'),
+    ('2', 'Task'),
+    ('3', 'User Story'),
+)
+
+
+class Issue(models.Model):
+    title = models.CharField(max_length=100, blank=False, default='')
+    slug = models.SlugField(max_length=100, unique=True)
+    created_by = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='issues',
+        blank=False,
+        editable=False
+    )
+    description = models.TextField(blank=False, default='')
+    status = models.IntegerField(choices=ISSUE_STATUS, default=0)
+    priority = models.IntegerField(choices=ISSUE_PRIORITY, default=0)
+    type = models.IntegerField(choices=ISSUE_TYPE, default=0)
+    created_on = models.DateTimeField(auto_now_add=True, blank=False, editable=False)
+    updated_on = models.DateTimeField(auto_now=True, blank=False, editable=False)
+    votes = models.ManyToManyField(User, related_name='votes', blank=True)
+    
+    class Meta:
+        ordering = ['priority', '-created_on', 'status']
+
+    def __str__(self):
+        return self.title
+    
+    def votes_number(self):
+        return self.votes.count()
+    
+
+class Comment(models.Model):
+    issue = models.ForeignKey(
+        Issue,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        blank=False,
+    )
+    created_by = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        blank=False,
+        editable=False
+    )
+    comment = models.TextField(blank=False, default='')
+    created_on = models.DateTimeField(auto_now_add=True, blank=False, editable=False)
+    updated_on = models.DateTimeField(auto_now=True, blank=False, editable=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return self.comment
