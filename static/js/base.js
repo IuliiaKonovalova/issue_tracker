a1d8b0d50254e6cfc8ea2ffae63c65afe86ab7fe
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-02-05 15:40:31 +0800

    Add functionality to dropwdown menu

--- static/js/base.js
+++ static/js/base.js
@@ -1,15 +1,26 @@
 const profileMenu = document.getElementById('dropdown-profile');
+const menu = document.querySelectorAll('.dropdown');
 const loginSignup = document.getElementById('dropdown-enter');
 const dropdown = document.querySelectorAll('.account__options');
 
+// On load
 document.addEventListener('DOMContentLoaded', function () {
+
+  // Close dropdown when the target is outside of the navbar container
   document.addEventListener('click', (e) => {
-    if ((e.target !== dropdown && e.target !== loginSignup) && loginSignup.classList.contains('open')) {
-      loginSignup.classList.remove('open');
+
+    if (document.getElementById('dropdown-profile')) {
+      if ((e.target !== dropdown && e.target !== profileMenu) && profileMenu.classList.contains('open')) {
+        profileMenu.classList.remove('open');
+      }
+    } else {
+      if ((e.target !== dropdown && e.target !== loginSignup) && loginSignup.classList.contains('open')) {
+        loginSignup.classList.remove('open');
+      }
     }
   })
-  loginSignup.addEventListener('click', () => {
-    loginSignup.classList.toggle('open');
-    console.log('click');
-  });
+  // Close open navbar menu
+  menu.forEach(m => m.addEventListener('click', () => {
+    m.classList.toggle('open');
+  }))
 });
