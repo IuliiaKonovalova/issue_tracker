101e19cef46aeec8bfc64ef423878ce091eaa336
lexach91 <ronmctailor@protonmail.com>
2022-02-08 16:27:26 +0800

    Add form for creating new projects and a simle view

--- projects/urls.py
+++ projects/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import ProjectsView
+from .views import ProjectsView, CreateProjectView
 
 urlpatterns = [
     path('', ProjectsView.as_view(), name='projects_list'),
+    path('new/', CreateProjectView.as_view(), name='create_project'),
 ]
