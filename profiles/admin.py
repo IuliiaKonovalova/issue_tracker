79e32b63ce5d0f08c2e53806680b3f56df53e085
lexach91 <ronmctailor@protonmail.com>
2022-02-12 14:42:13 +0800

    Add simple user profile view, template and url

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import UserProfile
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'username', 'email')
+    list_filter = ('user', 'username', 'email')
+    search_fields = ('user', 'username', 'email')
+    ordering = ('user', 'username', 'email')
+    list_select_related = ('user',)
