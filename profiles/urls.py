c9b4c5e382fdcc3d87b9f0fd3847819d936a7186
lexach91 <ronmctailor@protonmail.com>
2022-02-12 19:03:56 +0800

    Rewrite user profile view and template, remove UserProfile model

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,5 +3,5 @@ from .views import UserProfileView
 
 
 urlpatterns = [
-    path('', UserProfileView.as_view(), name='user_profile'),
+    path('<username>/', UserProfileView.as_view(), name='user_profile'),
 ]
