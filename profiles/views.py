a48c1bf02c895426ceb182598c7e8f0f4aad1b05
lexach91 <ronmctailor@protonmail.com>
2022-02-16 18:22:26 +0800

    Remove commented code from views.py

--- profiles/views.py
+++ profiles/views.py
@@ -11,28 +11,19 @@ class UserProfileView(View):
     def get(self, request, *args, **kwargs):
         user_profile = get_object_or_404(User, username=kwargs['username'])
         if request.user.is_authenticated:
-            # if user opens his own profile he can change his password
             if request.user == user_profile:
-                # if user is authenticated and is opening his own profile
-                # he can change his password
                 form = PasswordChangeForm(request.user)
-                # need to set autofocus to false to avoid autofocus on password field
                 form.fields['old_password'].widget.attrs['autofocus'] = False
                 return render(request, 'profiles/user_profile.html', {'user_profile': user_profile, 'password_form': form})
         return render(request, 'profiles/user_profile.html', {'user_profile': user_profile})
     
-    # post method for changing password
     def post(self, request, *args, **kwargs):
         user_profile = get_object_or_404(User, username=kwargs['username'])
         if request.user.is_authenticated:
-            # if user opens his own profile he can change his password
             if request.user == user_profile:
-                # if user is authenticated and is opening his own profile
-                # he can change his password
                 form = PasswordChangeForm(request.user, request.POST)
                 if form.is_valid():
                     user = form.save()
-                    # update the session to reflect the changes
                     update_session_auth_hash(request, user)
                     return HttpResponseRedirect(reverse('user_profile', kwargs={'username': user_profile.username}))
                 else:
