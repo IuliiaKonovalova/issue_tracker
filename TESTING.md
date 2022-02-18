0b1c345f3bb26b97d1faae9cfeb6dfac181a44fd
lexach91 <ronmctailor@protonmail.com>
2022-02-18 20:52:40 +0800

    Add code to specify esversion in base.js

--- TESTING.md
+++ TESTING.md
@@ -4,3 +4,5 @@
 ## Validation
 
 - ### HTML
+
+    + [Official HTML validator](https://validator.w3.org/) was used to validate the HTML output. No errors were found. 
--- static/js/base.js
+++ static/js/base.js
@@ -1,25 +1,32 @@
-const profileMenu = document.getElementById('dropdown-profile');
-const menu = document.querySelectorAll('.dropdown');
-const loginSignup = document.getElementById('dropdown-enter');
-const dropdown = document.querySelectorAll('.account__options');
-const openProjectTypes = document.getElementById('add-new-project');
-const addNewProject = document.getElementById('add__project');
-const projectOptions = document.getElementById('project__options');
-const addProjectIcon = document.getElementById('add-project-icon');
+/*jshint esversion: 6 */
+const profileMenu = document.getElementById("dropdown-profile");
+const menu = document.querySelectorAll(".dropdown");
+const loginSignup = document.getElementById("dropdown-enter");
+const dropdown = document.querySelectorAll(".account__options");
+const openProjectTypes = document.getElementById("add-new-project");
+const addNewProject = document.getElementById("add__project");
+const projectOptions = document.getElementById("project__options");
+const addProjectIcon = document.getElementById("add-project-icon");
 
 // On load
-document.addEventListener('DOMContentLoaded', function () {
-
+document.addEventListener("DOMContentLoaded", function () {
   // Close dropdown when the target is outside of the navbar container
-  document.addEventListener('click', (e) => {
-
-    if (document.getElementById('dropdown-profile')) {
-      if ((e.target !== dropdown && e.target !== profileMenu) && profileMenu.classList.contains('open')) {
-        profileMenu.classList.remove('open');
+  document.addEventListener("click", (e) => {
+    if (document.getElementById("dropdown-profile")) {
+      if (
+        e.target !== dropdown &&
+        e.target !== profileMenu &&
+        profileMenu.classList.contains("open")
+      ) {
+        profileMenu.classList.remove("open");
       }
     } else {
-      if ((e.target !== dropdown && e.target !== loginSignup) && loginSignup.classList.contains('open')) {
-        loginSignup.classList.remove('open');
+      if (
+        e.target !== dropdown &&
+        e.target !== loginSignup &&
+        loginSignup.classList.contains("open")
+      ) {
+        loginSignup.classList.remove("open");
       }
     }
 
@@ -27,14 +34,14 @@ document.addEventListener('DOMContentLoaded', function () {
     if (e.target === openProjectTypes || e.target === addProjectIcon) {
       projectTypes(e);
     }
-  })
+  });
 
   // Close open navbar menu
-  menu.forEach(m => m.addEventListener('click', () => {
-    m.classList.toggle('open');
-  }))
-
-
+  menu.forEach((m) =>
+    m.addEventListener("click", () => {
+      m.classList.toggle("open");
+    })
+  );
 });
 
 /**
@@ -42,6 +49,6 @@ document.addEventListener('DOMContentLoaded', function () {
  */
 const projectTypes = function (e) {
   e.preventDefault();
-  projectOptions.classList.remove('project__options--hidden');
-  addNewProject.classList.add('add__project--hidden');
+  projectOptions.classList.remove("project__options--hidden");
+  addNewProject.classList.add("add__project--hidden");
 };
