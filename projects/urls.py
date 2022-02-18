17ed9c345e95448e8891128414b89aab292fda74
lexach91 <ronmctailor@protonmail.com>
2022-02-18 21:21:04 +0800

    Format projects/urls.py to pass pep8 validation

--- projects/urls.py
+++ projects/urls.py
@@ -18,17 +18,69 @@ from .views import (
 
 urlpatterns = [
     path('', ProjectsView.as_view(), name='projects_list'),
-    path('new_personal_project/', CreatePersonalProjectView.as_view(), name='create_personal_project'),
-    path('new_team_project/', CreateTeamProjectView.as_view(), name='create_team_project'),
-    path('<slug:created_by>/<int:pk>/', ProjectDetailView.as_view(), name='project_detail'),
-    path('<slug:created_by>/<int:project_id>/new_issue/', CreateIssueView.as_view(), name='create_issue'),
-    path('<slug:created_by>/<int:project_id>/<int:issue_id>/', IssueDetailView.as_view(), name='issue_detail'),
-    path('<slug:created_by>/<int:project_id>/<int:issue_id>/vote/', IssueVotesView.as_view(), name='issue_vote'),
-    path('<slug:created_by>/<int:project_id>/edit/', EditProjectView.as_view(), name='edit_project'),
-    path('<slug:created_by>/<int:project_id>/<int:issue_id>/edit/', EditIssueView.as_view(), name='edit_issue'),
-    path('update_issue_status/', UpdateIssueStatusAjaxView.as_view(), name='update_issue_status'),
-    path('<slug:created_by>/<int:project_id>/delete/', DeleteProjectView.as_view(), name='delete_project'),
-    path('<slug:created_by>/<int:project_id>/<int:issue_id>/delete/', DeleteIssueView.as_view(), name='delete_issue'),
-    path('update_comment/', UpdateCommentAjaxView.as_view(), name='update_comment'),
-    path('delete_comment/', DeleteCommentAjaxView.as_view(), name='delete_comment'),
+    path(
+        'new_personal_project/',
+        CreatePersonalProjectView.as_view(),
+        name='create_personal_project'
+        ),
+    path(
+        'new_team_project/',
+        CreateTeamProjectView.as_view(),
+        name='create_team_project'
+        ),
+    path(
+        '<slug:created_by>/<int:pk>/',
+        ProjectDetailView.as_view(),
+        name='project_detail'
+        ),
+    path(
+        '<slug:created_by>/<int:project_id>/new_issue/',
+        CreateIssueView.as_view(),
+        name='create_issue'
+        ),
+    path(
+        '<slug:created_by>/<int:project_id>/<int:issue_id>/',
+        IssueDetailView.as_view(),
+        name='issue_detail'
+        ),
+    path(
+        '<slug:created_by>/<int:project_id>/<int:issue_id>/vote/',
+        IssueVotesView.as_view(),
+        name='issue_vote'
+        ),
+    path(
+        '<slug:created_by>/<int:project_id>/edit/',
+        EditProjectView.as_view(),
+        name='edit_project'
+        ),
+    path(
+        '<slug:created_by>/<int:project_id>/<int:issue_id>/edit/',
+        EditIssueView.as_view(),
+        name='edit_issue'
+        ),
+    path(
+        'update_issue_status/',
+        UpdateIssueStatusAjaxView.as_view(),
+        name='update_issue_status'
+        ),
+    path(
+        '<slug:created_by>/<int:project_id>/delete/',
+        DeleteProjectView.as_view(),
+        name='delete_project'
+        ),
+    path(
+        '<slug:created_by>/<int:project_id>/<int:issue_id>/delete/',
+        DeleteIssueView.as_view(),
+        name='delete_issue'
+        ),
+    path(
+        'update_comment/',
+        UpdateCommentAjaxView.as_view(),
+        name='update_comment'
+        ),
+    path(
+        'delete_comment/',
+        DeleteCommentAjaxView.as_view(),
+        name='delete_comment'
+        ),
 ]
