f70f215d6e1719dc83f6b442115924ae387d0fcf
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-02-18 20:57:23 +0800

    Format admin.py in projects app

--- projects/admin.py
+++ projects/admin.py
@@ -2,10 +2,18 @@ from django.contrib import admin
 from .models import Issue, Comment, Project
 from django_summernote.admin import SummernoteModelAdmin
 
+
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     summernote_fields = ('description',)
-    list_display = ('title', 'project_type', 'created_by', 'created_on', 'updated_on', 'status', )
+    list_display = (
+      'title',
+      'project_type',
+      'created_by',
+      'created_on',
+      'updated_on',
+      'status', 
+    )
     list_filter = ('created_on',)
     search_fields = ('name', 'created_by')
     ordering = ('-created_on',)
@@ -17,7 +25,15 @@ class ProjectAdmin(admin.ModelAdmin):
 @admin.register(Issue)
 class IssueAdmin(SummernoteModelAdmin):
     summernote_fields = ('description',)
-    list_display = ('title', 'created_by', 'created_on', 'status', 'priority', 'issue_type', 'votes_number')
+    list_display = (
+      'title',
+      'created_by',
+      'created_on',
+      'status',
+      'priority',
+      'issue_type',
+      'votes_number',
+    )
     list_filter = ('created_on', 'status', 'priority', 'issue_type')
     search_fields = ('title', 'description')
     ordering = ('-created_on', 'status', 'priority')
