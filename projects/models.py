0598df78f1eabcc9b435ef3a2e75f046da8f5995
lexach91 <ronmctailor@protonmail.com>
2022-02-18 21:17:16 +0800

    Format projects/models.py to pass pep8 validation

--- projects/models.py
+++ projects/models.py
@@ -37,22 +37,33 @@ ISSUE_TYPE = (
 
 
 class Project(models.Model):
-    title = models.CharField(max_length=35, blank=False, default='Untitled Project')
+    title = models.CharField(
+        max_length=35,
+        blank=False,
+        default='Untitled Project'
+        )
     # slug = models.SlugField(max_length=100, blank=False)
     description = models.TextField(max_length=300, blank=True)
     project_type = models.IntegerField(choices=PROJECT_TYPE)
     status = models.IntegerField(choices=PROJECT_STATUS, default=0)
-    collaborators = models.ManyToManyField(User, related_name='collaborated_projects')
-    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='projects', null=True)
+    collaborators = models.ManyToManyField(
+        User,
+        related_name='collaborated_projects'
+        )
+    created_by = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        related_name='projects',
+        null=True
+        )
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
-    
+
     class Meta:
         ordering = ['-created_on']
-        
+
     def __str__(self):
         return self.title
-    
 
 
 class Issue(models.Model):
@@ -69,8 +80,17 @@ class Issue(models.Model):
     issue_type = models.IntegerField(choices=ISSUE_TYPE, default=0)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
-    votes = models.ManyToManyField(User, related_name='voted_on_issue', blank=True)
-    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='issues', default=DEFAULT_PROJECT_ID)
+    votes = models.ManyToManyField(
+        User,
+        related_name='voted_on_issue',
+        blank=True
+        )
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE,
+        related_name='issues',
+        default=DEFAULT_PROJECT_ID
+        )
     assigned_to = models.ForeignKey(
         User,
         on_delete=models.SET_NULL,
@@ -78,16 +98,16 @@ class Issue(models.Model):
         null=True,
         related_name='assigned_issues'
     )
-    
+
     class Meta:
         ordering = ['priority', 'status']
 
     def __str__(self):
         return self.title
-    
+
     def votes_number(self):
         return self.votes.count()
-    
+
 
 class Comment(models.Model):
     issue = models.ForeignKey(
@@ -102,8 +122,16 @@ class Comment(models.Model):
         related_name='written_comments',
     )
     comment_body = models.TextField(max_length=300, blank=False, default='')
-    created_on = models.DateTimeField(auto_now_add=True, blank=False, editable=False)
-    updated_on = models.DateTimeField(auto_now=True, blank=False, editable=False)
+    created_on = models.DateTimeField(
+        auto_now_add=True,
+        blank=False,
+        editable=False
+        )
+    updated_on = models.DateTimeField(
+        auto_now=True,
+        blank=False,
+        editable=False
+        )
 
     class Meta:
         ordering = ['created_on']
