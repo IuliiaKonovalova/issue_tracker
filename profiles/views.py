bb7b4caf0cb9ac4ce1697a8d888b2aa85a9d29df
lexach91 <ronmctailor@protonmail.com>
2022-02-18 21:09:36 +0800

    Format profiles/views.py to pass pep8 validation

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render, get_object_or_404, reverse
-from django.views import  View
+from django.views import View
 from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth import update_session_auth_hash
-
 
 
 class UserProfileView(View):
@@ -14,9 +13,17 @@ class UserProfileView(View):
             if request.user == user_profile:
                 form = PasswordChangeForm(request.user)
                 form.fields['old_password'].widget.attrs['autofocus'] = False
-                return render(request, 'profiles/user_profile.html', {'user_profile': user_profile, 'password_form': form})
-        return render(request, 'profiles/user_profile.html', {'user_profile': user_profile})
-    
+                return render(
+                    request,
+                    'profiles/user_profile.html',
+                    {'user_profile': user_profile, 'password_form': form}
+                    )
+        return render(
+            request,
+            'profiles/user_profile.html',
+            {'user_profile': user_profile}
+            )
+
     def post(self, request, *args, **kwargs):
         user_profile = get_object_or_404(User, username=kwargs['username'])
         if request.user.is_authenticated:
@@ -25,7 +32,19 @@ class UserProfileView(View):
                 if form.is_valid():
                     user = form.save()
                     update_session_auth_hash(request, user)
-                    return HttpResponseRedirect(reverse('user_profile', kwargs={'username': user_profile.username}))
+                    return HttpResponseRedirect(
+                        reverse(
+                            'user_profile',
+                            kwargs={'username': user_profile.username})
+                            )
                 else:
-                    return render(request, 'profiles/user_profile.html', {'user_profile': user_profile, 'password_form': form})
-        return render(request, 'profiles/user_profile.html', {'user_profile': user_profile})
+                    return render(
+                        request,
+                        'profiles/user_profile.html',
+                        {'user_profile': user_profile, 'password_form': form}
+                        )
+        return render(
+            request,
+            'profiles/user_profile.html',
+            {'user_profile': user_profile}
+            )
