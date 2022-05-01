d10b070509afd7af89f42bdfd895fdaf789b45a3
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-05-01 15:46:30 +0800

    Add if statement for the databases used

--- issue_tracker/settings.py
+++ issue_tracker/settings.py
@@ -105,18 +105,19 @@ WSGI_APPLICATION = 'issue_tracker.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-DATABASES = {
+if "DATABASE_URL" in os.environ:
+    print("database = PostgreSQL via Heroku")
+    DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}
+    }
+else:
+    print("database = db.sqlite3")
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
